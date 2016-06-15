ed47a17db9be119e30d643003340d5839eb93988
Jonathan Colt <jonathan.colt@jivesoftware.com>
2016-06-14 21:41:18 -1000

    UI tweaks

--- upena-deployable/src/main/java/com/jivesoftware/os/upena/deployable/region/HealthPluginRegion.java
+++ upena-deployable/src/main/java/com/jivesoftware/os/upena/deployable/region/HealthPluginRegion.java
@@ -609,8 +609,8 @@ public class HealthPluginRegion implements PageRegion<HealthPluginRegion.HealthP
                                 UpenaEndpoints.shortHumanReadableUptime(System.currentTimeMillis() - recency)
                                 : ">" + UpenaEndpoints.shortHumanReadableUptime(System.currentTimeMillis() - startupTime);
 
-                            float hh = (float) Math.max(0, nodeHealth.health);
-                            hostRows.get(hi).get(0).put("color", "#" + getHEXTrafficlightColor(hh, 1f));
+                            //float hh = (float) Math.max(0, nodeHealth.health);
+                            hostRows.get(hi).get(0).put("color", "#d7d7d7");// + getHEXTrafficlightColor(hh, 1f));
                             hostRows.get(hi).get(0).put("host", nodeHealth.host); 
                             hostRows.get(hi).get(0).put("hostKey", nodeHealth.hostKey);
                             hostRows.get(hi).get(0).put("health", host.toString().replace(":", " "));
