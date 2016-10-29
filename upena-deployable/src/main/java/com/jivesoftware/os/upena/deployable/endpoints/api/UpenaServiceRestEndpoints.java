6615b27238918014a32a20853f250f8582058bef
Jonathan Colt <jonathan.colt@jivesoftware.com>
2016-10-28 16:25:16 -1000

    Cleanup endpoints

--- upena-deployable/src/main/java/com/jivesoftware/os/upena/deployable/endpoints/api/UpenaServiceRestEndpoints.java
+++ upena-deployable/src/main/java/com/jivesoftware/os/upena/deployable/endpoints/api/UpenaServiceRestEndpoints.java
@@ -18,8 +18,6 @@ package com.jivesoftware.os.upena.deployable.endpoints.api;
 import com.jivesoftware.os.mlogger.core.MetricLogger;
 import com.jivesoftware.os.mlogger.core.MetricLoggerFactory;
 import com.jivesoftware.os.routing.bird.shared.ResponseHelper;
-import com.jivesoftware.os.upena.service.DiscoveredRoutes;
-import com.jivesoftware.os.upena.service.UpenaService;
 import com.jivesoftware.os.upena.service.UpenaStore;
 import com.jivesoftware.os.upena.shared.Service;
 import com.jivesoftware.os.upena.shared.ServiceFilter;
@@ -40,16 +38,10 @@ public class UpenaServiceRestEndpoints {
 
     private static final MetricLogger LOG = MetricLoggerFactory.getLogger();
     private final UpenaStore upenaStore;
-    private final UpenaService upenaService;
-    private final DiscoveredRoutes discoveredRoutes;
-
-    public UpenaServiceRestEndpoints(@Context UpenaStore upenaStore,
-        @Context UpenaService upenaService,
-        @Context DiscoveredRoutes discoveredRoutes) {
+   
+    public UpenaServiceRestEndpoints(@Context UpenaStore upenaStore) {
         this.upenaStore = upenaStore;
-        this.upenaService = upenaService;
-        this.discoveredRoutes = discoveredRoutes;
-    }
+   }
 
 
     @POST
