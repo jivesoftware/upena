ab5438aecaa1ce9c7c58aad368bcb3dbe93c5584
Bruce Downs <bruce.downs@jivesoftware.com>
2017-09-21 13:13:04 -0700

    release script cleanup

--- release.sh
+++ release.sh
@@ -1,21 +1,17 @@
-#!/bin/bash
-
-
+#!/bin/sh
 
 echo "/-------------------------------------------------------"
 echo "| checking running from develop branch. "
 echo "\-------------------------------------------------------"
 ON_BRANCH=`git branch | grep "^*" | awk '{ print $2}'`
-if [ "$ON_BRANCH" != "develop" ]
-then
-	echo "You are release from branch '${ON_BRANCH}' which typically should be 'develop'. Are you sure you want to continue?r: y/n: "
+if [ "$ON_BRANCH" != "develop" ]; then
+	echo "You are release from branch '${ON_BRANCH}' which typically should be 'develop'. Are you sure you want to continue?: y/n: "
 	read ANSWER
 	if [ "$?" -ne "0" ]; then
-		exit 1;
+		exit 1
 	fi
-	if [ "$ANSWER" != "y" ]
-	then
-		exit 1;
+	if [ "$ANSWER" != "y" ]; then
+		exit 1
 	fi
 fi
 
@@ -23,29 +19,23 @@ echo "/-------------------------------------------------------"
 echo "| checking for outstanding commits"
 echo "\-------------------------------------------------------"
 OUT_STANDING_COMMITS=`git status | grep "nothing to commit" | wc -l | tr -d ' '`
-if [ "$OUT_STANDING_COMMITS" != "1" ]
-then
+if [ "$OUT_STANDING_COMMITS" != "1" ]; then
 	UNTRACKED=`git status | grep "nothing added to commit" | wc -l | tr -d ' '`
-	if [ "$UNTRACKED" != "1" ]
-	then
-		git status		
+	if [ "$UNTRACKED" != "1" ]; then
+		git status
 		echo "You have untracked files. Do you want to release anyway?: y/n: "
 		read ANSWER
 		if [ "$?" -ne "0" ]; then
-			cd -
-			exit 1;
+			exit 1
 		fi
-		if [ "$ANSWER" != "y" ]
-		then
-			cd -
-			exit 1;
+		if [ "$ANSWER" != "y" ]; then
+			exit 1
 		fi
-	else 
-		cd -
+	else
 		echo "/-------------------------------------------------------"
 		echo "| you have outstanding commits. Release cannot proceed until all commits are pushed."
 		echo "\-------------------------------------------------------"
-		exit 1;	
+		exit 1
 	fi
 fi
 
@@ -53,10 +43,8 @@ git checkout master
 git pull
 git checkout ${ON_BRANCH}
 
-
 VERSION=`cat pom.xml | grep -m 1 "<version>" | awk -F '[>-]' '{print $2}'`
 NEXT_VERSION=`echo $VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
-
 
 echo "/-------------------------------------------------------"
 echo "| setting version to "${VERSION}
@@ -84,7 +72,6 @@ if [ "$?" -ne "0" ]; then
 	exit 1
 fi
 
-
 git checkout ${ON_BRANCH}
 echo "/-------------------------------------------------------"
 echo "| setting version to "${NEXT_VERSION}"-SNAPSHOT on branch "${ON_BRANCH}
@@ -97,5 +84,4 @@ git push origin ${ON_BRANCH}
 echo "/-------------------------------------------------------"
 echo "| populating .m2 for "${NEXT_VERSION}"-SNAPSHOT"
 echo "\-------------------------------------------------------"
-mvn clean install -DskipTests
-
+mvn clean install
